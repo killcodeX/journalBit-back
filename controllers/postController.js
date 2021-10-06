339495e987ba7dbd931f79f02b1fef99c4135b18
Aaquib Ahmed <aaquibskimbox@gmail.com>
2021-10-06 14:21:45 +0530

    filter post created

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,4 +1,5 @@
 import PostMessage from "../models/post.js";
+import UserMessage from "../models/user.js";
 import { getMetData } from "../helpers/metscrapper.js";
 
 // POST Controllers
@@ -8,11 +9,32 @@ export const newPost = async (req, res) => {
   try {
     const result = await PostMessage.create({
       ...data,
-      topic:post.topic,
-      postedBy:req.userId
-    })
-    let newData = await PostMessage.find(result._id).populate("postedBy", "id fname lname avatar")
+      topic: post.topic,
+      postedBy: req.userId,
+    });
+    let newData = await PostMessage.find(result._id).populate(
+      "postedBy",
+      "id fname lname avatar"
+    );
     res.status(200).json({ result: newData });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const filterPost = async (req, res) => {
+  const { topic } = req.body;
+  try {
+    let data = await UserMessage.findById(req.userId);
+    let result = await PostMessage.find({
+      postedBy: { $in: [...data.following, req.userId] },
+    })
+      .populate("postedBy comments.postedBy", "id fname lname avatar work")
+      .sort("-createdAt");
+    let finalResult = result.filter(
+      (post) => post.topic == topic.toLowerCase()
+    );
+    res.status(200).json({ result: finalResult });
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
